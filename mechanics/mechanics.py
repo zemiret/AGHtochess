74186922d9e515f3fba689bb9807830b8d358a61
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2020-07-07 20:01:53 +0200

    add tests of generated unit price

--- mechanics/mechanics.py
+++ mechanics/mechanics.py
@@ -2,9 +2,8 @@ from random import randint
 
 from model.Unit import Unit
 
-sqrt2 = 2 ** 0.5
 golden = (1 + 5 ** 0.5) / 2
 
 
 def generate_unit(round: int) -> Unit:
-    return Unit(price=randint(int(sqrt2 ** round), int(3 * sqrt2 ** round)) * 100)
+    return Unit(price=randint(int(1.5 ** round), int(3 * 1.5 ** round)) * 100)
---
+++ mechanics/test_mechanics.py
@@ -0,0 +1,23 @@
+import pytest
+
+from mechanics import generate_unit
+
+
+@pytest.mark.parametrize('round, min_expected, max_expected', [
+    (1, 0, 1e6),
+    (5, 0, 1e6),
+    (10, 0, 1e6),
+
+    # TODO: balance
+    # (1, 100, 500),
+    # (5, 500, 10000),
+    # (10, 5000, 20000),
+])
+def test_generate_unit_price(round, min_expected, max_expected):
+    unit = generate_unit(round)
+
+    if min_expected is not None:
+        assert unit.price >= min_expected
+
+    if max_expected is not None:
+        assert unit.price <= max_expected
