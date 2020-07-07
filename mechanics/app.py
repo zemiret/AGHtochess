531dd41554ddb4b94b6e454b37684084449a0959
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2020-07-07 22:32:34 +0200

    whoever has more units, wins

--- mechanics/app.py
+++ mechanics/app.py
@@ -3,6 +3,7 @@ from random import randint
 from flask import Flask, request
 
 from mechanics import generate_unit
+from model.Board import Board
 
 app = Flask(__name__)
 
@@ -22,8 +23,11 @@ def generate_units_handler():
 def resolve_battle_handler():
     data = request.get_json()
 
+    board1 = Board.from_dict(data['player_1'])
+    board2 = Board.from_dict(data['player_2'])
+
     return {
-        "winner": randint(1, 2),
+        "winner": 1 if len(board1.tokens) > len(board2.tokens) else 2,
         "player_hp_change": randint(-10, 0),
         "log": [
             {
