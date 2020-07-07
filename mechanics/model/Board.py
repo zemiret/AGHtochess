d1f1a4fb7a6e2a1dd04343592ccca7bc4d938d4e
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2020-07-07 22:30:16 +0200

    implement Board.from_dict with tests

--- mechanics/model/Board.py
+++ mechanics/model/Board.py
@@ -2,8 +2,19 @@ from dataclasses import dataclass
 from typing import List
 
 from model.Token import Token
+from model.Unit import Unit
 
 
 @dataclass(eq=True)
 class Board:
     tokens: List[Token]
+
+    @classmethod
+    def from_dict(cls, d: dict):
+        units = map(Unit.from_dict, d['units'])
+        units = {unit.id: unit for unit in units}
+        tokens = [
+            Token(units[placement['unit_id']], (placement['x'], placement['y']))
+            for placement in d['units_placement']
+        ]
+        return cls(tokens)
