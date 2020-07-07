e40b2be6a4d490a87bffc68f90b95ec847c2ca80
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2020-07-07 19:36:51 +0200

    add POST endpoint /generate_units

--- mechanics/app.py
+++ mechanics/app.py
@@ -1,6 +1,33 @@
-from flask import Flask
+from flask import Flask, request
+
+from model.Unit import Unit
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return {'foo': 123, 'bar': 456}
+
+@app.route('/generate_units', methods=['POST'])
+def generate_units():
+    data = request.get_json()
+    round: int = data['round']
+
+    # return {
+    #     "units": [
+    #         {
+    #             "id": 43,
+    #             "attack": 1,
+    #             "defense": 10,
+    #             "magic_resist": 10,
+    #             "critical_chance": 40,
+    #             "hp": 100,
+    #             "range": 40,
+    #             "attack_speed": 10,
+    #             "type": "MAGICAL/PHYSICAL",
+    #             "price": 300
+    #         }
+    #     ]
+    # }
+    return {
+        "units": [
+            Unit(hp=123),
+        ]
+    }
