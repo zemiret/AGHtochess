ddad1df48e4675254f5e70a5b12bb072495d9a72
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-07-08 23:52:19 +0200

    Minor timer fixes

--- frontend/src/components/GamePage.tsx
+++ frontend/src/components/GamePage.tsx
@@ -4,15 +4,26 @@ import { RootSchema } from "../store/root-schema";
 import { connect } from "react-redux";
 import Player from "./Player";
 import GamePhaseSpecific from "./GamePhaseSpecific";
+import Timer from "./Timer";
 
 interface Props {
   player: PlayerInfo;
   enemy: PlayerInfo;
+  phase: string;
+  round: number;
+  phaseEndsAt: number;
 }
 
-const GamePage: React.FunctionComponent<Props> = ({ player, enemy }: Props) => {
+const GamePage: React.FunctionComponent<Props> = ({
+  player,
+  enemy,
+  phase,
+  round,
+  phaseEndsAt,
+}: Props) => {
   return (
     <div>
+      <Timer phase={phase} phaseEndsAt={phaseEndsAt} round={round} />
       <h1>Player</h1>
       <Player {...player} />
       <h1>Enemy</h1>
@@ -26,6 +37,9 @@ const GamePage: React.FunctionComponent<Props> = ({ player, enemy }: Props) => {
 const mapStateToProps = ({ gameState }: RootSchema) => ({
   player: gameState!.player,
   enemy: gameState!.enemy,
+  phase: gameState!.phase,
+  round: gameState!.round,
+  phaseEndsAt: gameState!.phaseEndsAt,
 });
 
 export default connect(mapStateToProps)(GamePage);
