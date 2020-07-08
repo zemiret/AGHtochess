9d11ce1f4002e5feb3c6dd95b0e03ef27428bddd
amleczko <dev@amleczko.pl>
2020-07-08 19:55:31 +0200

    Add json questions loading

--- communicator/main.go
+++ communicator/main.go
@@ -9,15 +9,18 @@ import (
 const port = 4000
 
 func main() {
-
 	fmt.Printf("Starting communicator at :%d\n", port)
+	err := LoadQuestions()
+	if err != nil {
+		log.Fatal("Did not load questions", err)
+	}
 
 	hub := newHub()
 	go hub.run()
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
--- communicator/questions.go
+++ communicator/questions.go
@@ -1,19 +1,40 @@
 package main
 
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"log"
+	"math/rand"
+)
+
+const (
+	QUESTIONS_FILE = "assets/questions.json"
+)
+
+type Questions []*Question
+
+var questions Questions
+
 func GetQuestion() (*Question, error) {
-	return &Question{
-		ID:   1,
-		Text: "Na kogo głosujesz w drugiej turze",
-		Answers: []Answer{
-			{
-				ID:   1,
-				Text: "Czaskoski",
-			},
-			{
-				ID:   2,
-				Text: "Dupa",
-			},
-		},
-		CorrectAnswer: 1,
-	}, nil
+	if questions == nil {
+		return nil, errors.New("questions not loaded")
+	}
+	return questions[rand.Intn(len(questions))], nil
+}
+
+
+func LoadQuestions() error {
+	content, err := ioutil.ReadFile(QUESTIONS_FILE)
+	if err != nil {
+		log.Printf("Error loading file: %s", QUESTIONS_FILE)
+		return err
+	}
+
+	if err != json.Unmarshal(content, &questions) {
+		log.Printf("Error unmarshalling file: %s", QUESTIONS_FILE)
+		return err
+	}
+
+	return nil
 }
