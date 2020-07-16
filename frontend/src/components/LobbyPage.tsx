8b74d12db035ba7b3018fdc7c20907caad9f2fb4
amleczko <dev@amleczko.pl>
2020-07-16 22:46:07 +0200

    WIP: overflow testing

--- frontend/src/components/LobbyPage.tsx
+++ frontend/src/components/LobbyPage.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import { Container, Jumbotron, Spinner, Row } from "reactstrap";
 
 const LobbyPage: React.FunctionComponent = () => {
+  const augmentedClips = "exe t-clip-x b-clip-x";
+
   return (
     <Container>
-      <Jumbotron className="my-4 mx-5">
-        <h1 className="display-4">Lobby</h1>
-        <Row className="justify-content-center">
-          <Spinner className="my-4" />
-        </Row>
-      </Jumbotron>
+      <div className="lobby-container">
+        <Jumbotron className="my-4 mx-5" augmented-ui={augmentedClips}>
+          <h1 className="display-4">Scanning the galaxy to find your enemy...</h1>
+          <Row className="justify-content-center">
+            <Spinner className="my-4" />
+          </Row>
+        </Jumbotron>
+      </div>
     </Container>
   );
 };
